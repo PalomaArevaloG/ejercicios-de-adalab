e1ba30e51e1c123c7729faa1473fbc2dd811cc63
Paloma <paloma.arevalog@gmail.com>
2021-08-20 17:56:23 +0200

    20aug

---
+++ modulo-2/modulo-2-leccion-1/modulo-2-leccion-1-ejercicio-4-seleccionando adalabers/main.js
@@ -0,0 +1,4 @@
+"use strict";
+const seleccionada = document.querySelector(".adalaber2");
+const titulo = document.querySelector(".title");
+titulo.innerHTML = titulo.innerHTML + seleccionada.innerHTML;
---
+++ modulo-2/modulo-2-leccion-1/modulo-2-leccion-1-ejercicio-8-botones/main.js
@@ -0,0 +1,6 @@
+"use strict";
+
+const bebe = document.querySelector(".bebe");
+const come = document.querySelector(".come");
+bebe.classList.remove("opacity");
+come.classList.add("opacity");
