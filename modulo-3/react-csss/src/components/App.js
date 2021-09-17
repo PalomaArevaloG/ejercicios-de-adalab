622137dfefbb2112d90a284cdee24d0d28a1d08a
Paloma <paloma.arevalog@gmail.com>
2021-09-17 08:48:39 +0200

    17sep

---
+++ modulo-3/react-csss/src/components/App.js
@@ -0,0 +1,16 @@
+// Fichero src/components/App.js
+import '../styles/App.css';
+
+function App() {
+    return ( <
+        div >
+        <
+        header >
+        <
+        h1 className = "title" > Título de mi página < /h1>{' '} <
+        /header>{' '} <
+        /div>
+    );
+}
+
+export default App;
---
+++ modulo-3/react-starter-kit - copia/src/components/App.js
@@ -0,0 +1,13 @@
+// Fichero src/components/App.js
+import '../styles/App.scss';
+
+function App() {
+    return ( <
+        div >
+        <
+        h1 > Hola mundo < /h1>{' '} <
+        /div>
+    );
+}
+
+export default App;
